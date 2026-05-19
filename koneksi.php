9d2e9b392f1f8592e4f372a8320bcb4b24b53e8f
manucamputuak-maker <manucampurtuak@gmail.com>
2026-05-20 01:20:31 +0700

    fix koneksi railway mysql

--- koneksi.php
+++ koneksi.php
@@ -6,10 +6,8 @@ $password = getenv("MYSQLPASSWORD") ?: "";
 $database = getenv("MYSQLDATABASE") ?: "db_admin";
 $port = getenv("MYSQLPORT") ?: 3306;
 
-// Membuat koneksi
 $conn = mysqli_connect($host, $username, $password, $database, $port);
 
-// Cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
